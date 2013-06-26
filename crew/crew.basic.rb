821f0f2f1527177b5f33752e1dc3b23755c33bc6
Derk-Jan Karrenbeld <derk-jan+git@karrenbeld.info>
2013-06-27 01:41:03 +0200

    Add message printing

--- crew/crew.basic.rb
+++ crew/crew.basic.rb
@@ -6,16 +6,26 @@ class BasicCrew
 		"MyBasicCrew 1.0.0"
 	end
 	
+	def initialize
+		@last_result = nil
+	end
+	
 	#
 	#
 	def board( ship )
 		@ship = ship
+		@ship.queue @ship.scan()
 	end
 	
 	#
 	#
 	def step( t )
-	
+		
+		if @last_result != @ship.result
+			@last_result = @ship.result
+			Space.timestamped t, @last_result
+		end
+		
 	end
 	
 	#
