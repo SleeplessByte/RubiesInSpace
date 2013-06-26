21c5d541ad4c849ed53e1eef248bbe862a5f0b56
Derk-Jan Karrenbeld <derk-jan+git@karrenbeld.info>
2013-06-26 23:14:21 +0200

    Add Basic Crew

---
+++ crew/crew.basic.rb
@@ -0,0 +1,27 @@
+class BasicCrew
+
+	#
+	#
+	def identifier
+		"MyBasicCrew 1.0.0"
+	end
+	
+	#
+	#
+	def board( ship )
+		@ship = ship
+	end
+	
+	#
+	#
+	def step( t )
+	
+	end
+	
+	#
+	#
+	def event( event )
+	
+	end
+	
+end
