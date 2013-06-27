9e898306df68a6bf98fcfacdf9a26fc5db131293
Derk-Jan Karrenbeld <derk-jan+git@karrenbeld.info>
2013-06-27 07:21:22 +0200

    Better logs.
    Temp until WERD.

--- generator/log.space.rb
+++ generator/log.space.rb
@@ -28,18 +28,24 @@ class SpaceLog
 		#tech = scan.source.tech
 		
 		# TODO use werd to generate nice logs, use tech
-		"#{ scan.source }. I was gazing through my scanner when I looked upon the surroundings. #{ scan.environment[ :log ] } In the distance I could see #{ scan.paths.length } bodies, but they were too far to make out. #{ scan.friendlies.length - 1 } other ships of the Alliance were enjoying the view, but not for much longer as from the horizon #{ scan.enemies.length } unknown ships were closing in..."
+		result = "#{ scan.source }. I was gazing through my scanner when I looked upon the surroundings. #{ scan.environment[ :log ] } In the distance I could see #{ scan.paths.length } bodies, but they were too far to make out. " 
+		
+		result += "#{ scan.friendlies.length - 1 } other ships of the Alliance were enjoying the view. " if scan.friendlies.length > 1
+		
+		result += "We can't stay much longer as from the horizon I spotted #{ scan.enemies.length } unknown ships closing in..." if scan.enemies.length > 0
+		
+		return result
 	end
 	
 	def self.generate_result_of_travel( travel )
 		
-		"#{ travel.source }. Pff #{ travel.distance } lightyears! That took us long enough. More than #{ travel.duration.floor } days flying. We consumed #{ travel.depletion.round } litres of deuterium. But we are finally there!"
+		"#{ travel.source }. Pff #{ travel.distance } lightyears! That took us long enough. More than #{ travel.duration.floor } days flying. We consumed #{ travel.depletion.round } litres of deuterium. But we are finally at #{ travel.node }!"
 		
 	end
 	
 	def self.generate_result_of_collect( collect )
 		
-		"#{ collect.source }. We've managed to collect #{ collect.collected } litres of deuterium."
+		"#{ collect.source }. We've managed to collect #{ collect.collected.floor } litres of deuterium."
 		
 	end
 	
