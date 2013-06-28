6a81baa64d76e0c7e4f341aac135a3061e588465
Derk-Jan Karrenbeld <derk-jan+git@karrenbeld.info>
2013-06-28 17:15:27 +0200

    Add Report Attack

--- interface/report.attack.rb
+++ interface/report.attack.rb
@@ -4,13 +4,14 @@ class ShipInterface
 
 	class AttackReport < CommandReport
 
-		attr_reader :destination, :inflicted, :received
+		attr_reader :destination
+		attr_accessor :damage, :depletion
 		
-		def initialize( timestamp, source, destination, inflicted, received )
+		def initialize( timestamp, source, destination, damage, depletion )
 			super timestamp, source
 			@destination = destination
-			@inflicted = inflicted
-			@received = received
+			@damage = damage
+			@depletion = depletion
 		end
 		
 	end
